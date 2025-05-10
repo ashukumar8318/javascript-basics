8378158d916a89d75ab52ef0e37bd1004c3900e0
ashukumar8318 <ashu.kumarag1998@gmail.com>
2025-05-10 17:19:06 +0530

    datatype

---
+++ basic/datatype.js
@@ -0,0 +1,7 @@
+console.log(typeof null); // OBJECT
+console.log(typeof undefined); //Undefined
+
+let commitmessage;  ///  it is undefined you can undefinned like this
+console.log(typeof commitmessage);
+
+
