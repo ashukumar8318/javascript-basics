b08caba4102d1f6aabd147c3bd1064f88b52b28f
ashukumar8318 <ashu.kumarag1998@gmail.com>
2025-05-10 17:40:59 +0530

    conversion

---
+++ basic/converteinNumber.js
@@ -0,0 +1,37 @@
+let score = "abc"
+console.log(typeof score);
+
+let score1 = "33"
+console.log(typeof score1);
+
+let valueinNumber = Number(score)  // u can conevert string into numbber by this
+console.log(typeof valueinNumber);
+console.log(valueinNumber);
+
+
+//33 => 33
+//"33"=> 33
+//"33abc"=>NaN not a number
+//true => 1
+//"abc" => Nan
+
+let isloggedin = "abc"
+let chekedin = Boolean(isloggedin)
+
+console.log(typeof chekedin);
+console.log(chekedin);
+
+ //true = 1
+ //false = 0
+ //""= false 
+ //"abc"= true
+ //1= true
+//0 =false
+
+
+
+
+
+
+
+
