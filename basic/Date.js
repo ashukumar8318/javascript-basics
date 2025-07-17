94d6cf5a92f055becc2a6a0403b869bfa4e9cc75
ashukumar8318 <ashu.kumarag1998@gmail.com>
2025-07-17 09:39:04 +0000

    date and time

---
+++ basic/Date.js
@@ -0,0 +1,18 @@
+// let date = new Date()
+// console.log(date.toString());
+// console.log(date.toDateString());
+// console.log(date.toISOString());
+// console.log(date.toLocaleTimeString());
+// console.log(date.toJSON());
+
+
+let myCreatedDate = new Date(2-10-1998)
+console.log(myCreatedDate.toDateString());
+
+let mycreatedDate2= Date.now()
+console.log(mycreatedDate2.toString());
+
+
+
+
+
--- basic/Number.js
+++ basic/Number.js
@@ -5,6 +5,12 @@ let newNum = 25/3;
 console.log(newNum.toFixed(2));
 
 // ++++++++++ Maths +++++++
-console.log(maths);
+console.log(Math);
 
-console.log(maths.abs(-6));
+console.log(Math.abs(-5));
+console.log(Math.round(4.6));
+console.log(Math.min(10,78,90));
+console.log(Math.max(78,119,890));
+
+
+
