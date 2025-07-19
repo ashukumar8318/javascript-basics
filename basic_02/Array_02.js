9c2727637cd8ba6e7245cfc8e5d37752a0b78886
ashukumar8318 <ashu.kumarag1998@gmail.com>
2025-07-19 13:15:45 +0000

    array_02

---
+++ basic_02/Array_02.js
@@ -0,0 +1,13 @@
+const marvel_heroes = ["Thor","Iron","Captain America"]
+
+const dc_heroes = ["Batman","superman","wonder"]
+
+
+marvel_heroes.push(dc_heroes);
+console.log(marvel_heroes);
+console.log(marvel_heroes[3][1]);
+
+
+
+
+
