adde20a53df97102cbfb410ac64d2c433c7e24eb
ashukumar8318 <ashu.kumarag1998@gmail.com>
2025-07-19 12:45:06 +0000

    arrays

---
+++ basic_02/Arrays.js
@@ -0,0 +1,15 @@
+let array = [1,2,3,4,5];
+console.log(array.length);
+
+
+let array1 = new Array(12,23,345,56,89);
+console.log(array1.length);
+
+array1.push(100);
+console.log(array1);
+console.log(array1.lenth);
+
+
+
+
+
