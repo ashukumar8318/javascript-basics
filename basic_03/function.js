ec6a8761764cda52597e41c44cc6a92486787a3b
ashukumar8318 <ashu.kumarag1998@gmail.com>
2025-07-31 11:35:56 +0000

    functions

---
+++ basic_03/function.js
@@ -0,0 +1,15 @@
+function addTwoNumber(a,b){
+    if(typeof a== "number" && typeof b== "number"){
+        return a + b
+    }
+    else{
+        console.log("please provide two number");
+        
+
+        
+    }
+}
+
+console.log(addTwoNumber(2,3));
+
+
