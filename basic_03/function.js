33c2e545ed05ada0e0c0b991fb94515a0d7f2120
ashukumar8318 <ashu.kumarag1998@gmail.com>
2025-08-04 15:06:17 +0000

    scope

--- basic_03/function.js
+++ basic_03/function.js
@@ -1,15 +1,51 @@
-function addTwoNumber(a,b){
-    if(typeof a== "number" && typeof b== "number"){
-        return a + b
-    }
-    else{
-        console.log("please provide two number");
+// function addTwoNumber(a,b){
+//     if(typeof a== "number" && typeof b== "number"){
+//         return a + b
+//     }
+//     else{
+//         console.log("please provide two number");
         
 
         
-    }
+//     }
+// }
+
+// console.log(addTwoNumber("2",3));
+
+
+function SayMyName(){
+    console.log("A");
+    console.log("S");
+    console.log("H");
+    console.log("U");
 }
 
-console.log(addTwoNumber(2,3));
+// SayMyName()
+// SayMyName()
 
 
+// function addTwonumbers(number1,number2){
+// console.log(number1+number2);
+
+// }
+//addTwonumbers(23,34)
+
+
+function addTwoNumber(number1,number2){
+    let result = number1+number2
+    return result
+    
+}
+let result = addTwoNumber(2,3)
+console.log("result:",result);
+
+
+function userLoggedIn(username){
+    if(!username){
+        return console.log("Please provide username");
+    }
+    return `${username} just logged in`
+
+}
+
+console.log(userLoggedIn());
---
+++ basic_03/scope.js
@@ -0,0 +1,18 @@
+function one(){
+const website = "javascript.com"
+const year = 23
+function two(){
+    const newlan = "c++"
+    console.log("long years ago");
+    console.log(website);
+    console.log(year);
+    
+}
+
+//console.log(newlan);
+
+two()
+
+}
+
+one()
