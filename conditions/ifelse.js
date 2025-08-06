d4356654b22ab64fd2c7cb48d9d1d5b8fbc8c6be
ashukumar8318 <ashu.kumarag1998@gmail.com>
2025-08-06 11:52:11 +0000

    switch

---
+++ conditions/ifelse.js
@@ -0,0 +1,16 @@
+ const balance= 1000
+
+ if(balance<500){
+    console.log("balance is less then 500");
+    
+ }
+ else if(balance<900){
+    console.log("balance i greater then 900");
+    
+ }
+ else{
+    console.log("balance is equal to 1000");
+    
+ }
+
+
---
+++ conditions/switch.js
@@ -0,0 +1,44 @@
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("jan");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march" :
+        console.log("march");
+        break;
+
+    default:
+        break;
+}
+
+
+
+//Nuliesh coelison operator  it is used in database calll (??)
+
+
+
+let val1;
+
+// val1 = 10 ?? 20
+
+// val1 = null ?? 20
+
+
+val1 = undefined ?? 30
+
+
+
+
+console.log(val1);
+
+
+
+
+
+
+
+
